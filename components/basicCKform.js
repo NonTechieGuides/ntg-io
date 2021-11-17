f7260399841db4bf65039c3f57662bda336476e4
Flaturtha <richthewriterguy@gmail.com>
2021-11-17 09:36:28 -0600

    moved everything inside /frontend

--- components/basicCKform.js
+++ components/basicCKform.js
@@ -1,13 +1,10 @@
-export default function BasicCK() {
+import ConvertKitForm from 'convertkit-react'
+
+const MY_FORM_ID = 2780208
+
+function CKForm() {
   return (
-    <form className='flex flex-col mx-auto mt-12 bg-gray-200 p-8'>
-      <h1 className="text-2xl font-bold text-center">Sign up!</h1>
-      <p className="text-xl text-center">Get notified as soon as the site is live!</p>
-	    <label htmlFor='name' className='mt-8 mb-2 italic'>Name</label>
-	    <input className='mb-2 border-b-2' id='name' name='name' type='text' autoComplete='name' required />
-      <label htmlFor='email' className='mb-2 italic'>Email</label>
-	    <input className='mb-4 border-b-2' id='email' name='email' type='email' autoComplete='email' required />
-	    <button type='submit' className='px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700'>Submit</button>
-	  </form>
+    <ConvertKitForm template="cocoa" borderRadius={10} formId={MY_FORM_ID} />
+
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Header from '../components/header'
-import CKForm from '../components/basicCKform'
+import ConvertKitForm from 'convertkit-react'
+
+const MY_FORM_ID = 2780208
 
 function HomePage() {
   return (
@@ -13,7 +15,7 @@ function HomePage() {
         <p><span className="font-bold">YOU CAN BUILD A GREAT WEBSITE &hellip;</span> and take advantage of the same blazing fast technologies that the heavy hitters use. Free tutorials, guides, and an A-to-Z book that'll explain it all in simple-to-understand plain English.</p>
         <p>Used by Peloton, Logger MTS, Louis Vuitton, Leonardo DiCaprio Foundation, TakeShape.io, Fabuwood, Right From Basics, Easyship, Ingmarson, JFK Terminal 4. <span className="text-gray-400 text-sm italic">(source:&nbsp;<a href="https://naturaily.com/blog/top-10-websites-built-on-jamstack">Naturally.com</a>)</span></p>
       </div>
-      <CKForm />
+      <ConvertKitForm template="cocoa" borderRadius={10} formId={MY_FORM_ID} />
     </div>
   </>
   )
